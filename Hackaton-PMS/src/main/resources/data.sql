42d77e21673672b70f5f9ad76cdaf0328c8a6126
Sharathrgowda <123438652+Sharathrgowda@users.noreply.github.com>
2023-07-26 15:28:52 +0530

    Delete Hackaton-PMS directory

--- Hackaton-PMS/src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT  INTO _user(id ,email,password) VALUES('50','admin@gmail.com', '$2a$10$etgRWiMByNELtJozbIQSXO3dTDN.5U.OKvJpVOCfo7s/jDrw/m/bC');
-
-INSERT	INTO employee_details(employee_id,employee_code,email_id,employee_name,mobile_number,designation,department,employee_grade,join_date,reporting_email_id,gender,location) values('f5d9d95e-b995-11ed-afa1-0242ac120002','000000100231','admin@gmail.com','Excel','74710 89573','ADMIN','admin','5','2010-07-25 18:44:38','admin@gmail.com','M','Mysore');
-
-INSERT INTO  skills(skill_id,skill_name) VALUES('effdaeba-c15c-11ed-afa1-0242ac120002','java'),('effdb1da-c15c-11ed-afa1-0242ac120002','python'),('effdb342-c15c-11ed-afa1-0242ac120002','C#'),('effdb4aa-c15c-11ed-afa1-0242ac120002','.net'),('effdb658-c15c-11ed-afa1-0242ac120002','AWS');
